caf7f26f5f73d538562b763234a39b883615bfbf
rim25x <rim25x@yandex.ru>
2022-07-19 01:06:13 +0500

    2 commit

--- src/FirstPac/Main.java
+++ src/FirstPac/Main.java
@@ -1,7 +1,37 @@
 package FirstPac;
 
+
+import java.util.Scanner;
+
 public class Main {
+    public static class CalcMe {
+        static public double ThisValues(double a, double b, char c) {
+            switch (c) {
+                case '+': {
+                    return a+b;
+                }
+                case '-': {
+                    return a-b;
+                }
+                case '/': {
+                    return a/b;
+                }
+                case '*': {
+                    return a*b;
+                }
+                default:
+                    System.out.print("Bye Bye! ");
+            }
+            return a+b;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input 2 values: ");
+        double num1 = 0; double num2 = 0; char c;
+        num1 = in.nextDouble();
+        c = in.next().charAt(0);
+        num2 = in.nextDouble();
+        System.out.println(CalcMe.ThisValues(num1, num2, c));
     }
 }
